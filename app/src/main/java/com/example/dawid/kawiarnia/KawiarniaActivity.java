39fd73b17b10f3dd7f737bf1fe55962ae5df03bc
dawidb1 <33998058+dawidb1@users.noreply.github.com>
2018-01-31 10:41:54 +0100

    init

---
+++ app/src/main/java/com/example/dawid/kawiarnia/KawiarniaActivity.java
@@ -0,0 +1,13 @@
+package com.example.dawid.kawiarnia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KawiarniaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kawiarnia);
+    }
+}
